2935bc790a3da7f31f7f80f9cf801e101955814b
Adarsh Jena <76236137+adarshnjena@users.noreply.github.com>
2024-07-12 02:28:36 +0530

    chore: Update STATIC_ROOT path in settings.py to use 'static' directory instead of 'media'

--- pixelated_image/settings.py
+++ pixelated_image/settings.py
@@ -22,7 +22,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'media')
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 
 # Quick-start development settings - unsuitable for production
